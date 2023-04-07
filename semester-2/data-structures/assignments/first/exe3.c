558502d176c2a3b8db1410a6d38fda4ec8bcc1f0
Antonis Anagnostou <anagnostouantonis78@gmail.com>
2023-04-08 02:05:56 +0300

    Create function to initialize cashDesks

--- semester-2/data-structures/assignments/first/exe3.c
+++ semester-2/data-structures/assignments/first/exe3.c
@@ -26,7 +26,26 @@ Customer customerCreate(int id, int items) {
     return cr;
 }
 
+CashDesk *cashDesksInit(void) {
+    int i, j;
+    static CashDesk cashDesks[NO_OF_CASH_DESKS];
+    for (i = 0; i < NO_OF_CASH_DESKS; i++)
+    {
+        for (j = 0; j < MAX_QUEUE_SIZE; j++)
+        {
+            cashDesks[i].customers[j] = customerCreate(j, j);
+        }
+        cashDesks[i].newSize = 0;
+        cashDesks[i].oldSize = 0;
+        cashDesks[i].frontIndex = 0;
+        cashDesks[i].rearIndex = 0;
+    }
+    return cashDesks;
+}
+
 int main(void)
 {
+    CashDesk *cashDesks = cashDesksInit();
+    
     return 0;
 }
