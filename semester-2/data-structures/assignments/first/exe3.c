79cc59ec0c39857970a29e6bc6bbc6fd8b6fffe2
Antonis Anagnostou <anagnostouantonis78@gmail.com>
2023-04-08 00:16:26 +0300

    Create structs for Customer and CashDesk

--- semester-2/data-structures/assignments/first/exe3.c
+++ semester-2/data-structures/assignments/first/exe3.c
@@ -6,6 +6,20 @@
 #define NO_OF_CUSTOMERS 60
 #define NO_OF_CASH_DESKS 3
 
+typedef struct {
+    int id;
+    int items;
+} Customer;
+
+typedef struct {
+    Customer customers[MAX_QUEUE_SIZE];
+    int fIndex;
+    int rIndex;
+    int old_size;
+    int new_size;
+} CashDesk;
+
+
 int main(void)
 {
     return 0;
