ee4228f5a1ff31245b11257bd55cf92da4f2e5ea
Antonis Anagnostou <anagnostouantonis78@gmail.com>
2023-04-08 12:08:02 +0300

    Create functions to add(enqueue) and remove(dequeue)

--- semester-2/data-structures/assignments/first/exe3.c
+++ semester-2/data-structures/assignments/first/exe3.c
@@ -49,7 +49,36 @@ bool cashDeskIsFull(CashDesk cd) {
     return ((cd.frontIndex == cd.rearIndex + 1) || (cd.frontIndex == 0 && cd.rearIndex == MAX_QUEUE_SIZE -1));
 }
 
+void cashDeskAdd(CashDesk *cd, Customer cm) {
+    if (!cashDeskIsFull(*cd)) {
+        if (cd->frontIndex == -1) cd->frontIndex = 0;
+        cd->rearIndex = (cd->rearIndex + 1) % MAX_QUEUE_SIZE;
+        cd->customers[cd->rearIndex] = cm;
+        cd->itemsCount++;
+    } else {
+        printf("Cash desk can't accept any more customers!");
+        printf("OVERFLOW! Shutting down...");
+        exit(1);
+    }
+};
 
+void cashDeskRemove(CashDesk *cd) {
+    if (!cashDeskIsEmpty(*cd)) {
+        if (cd->frontIndex == cd->rearIndex)
+        {
+            cd->frontIndex = -1;
+            cd->rearIndex = -1;
+            cd->itemsCount--;
+        } else {
+            cd->frontIndex = (cd->frontIndex + 1) % MAX_QUEUE_SIZE;
+            cd->itemsCount--;
+        }
+    } else {
+        printf("There are no customers to kick out of the queue!");
+        printf("UNDERFLOW! Shutting down...");
+        exit(1);
+    }
+}
 
 int main(void)
 {
