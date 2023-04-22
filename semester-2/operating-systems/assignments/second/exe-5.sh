d247fbe144462a02f90c2916d0e8dedb01212c47
Antonis Anagnostou <anagnostouantonis78@gmail.com>
2023-04-23 00:38:06 +0300

    Create a bash script to convert a single digit number to string

---
+++ semester-2/operating-systems/assignments/second/exe-5.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo -n "Enter a number (0-9) to convert to string: "
+read num
+case $num in
+    0) echo "Zero";;
+    1) echo "One";;
+    2) echo "Two";;
+    3) echo "Three";;
+    4) echo "Four";;
+    5) echo "Five";;
+    6) echo "Six";;
+    7) echo "Seven";;
+    8) echo "Eight";;
+    9) echo "Nine";;
+    *) echo "Unsupported range";;
+esac
