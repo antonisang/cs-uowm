a93d516f8779a62aac0591f2b3a14ab7209813e7
Antonis Anagnostou <anagnostouantonis78@gmail.com>
2023-05-13 14:35:27 +0300

    Define types student and treeNode

--- semester-2/data-structures/assignments/second/exe1.c
+++ semester-2/data-structures/assignments/second/exe1.c
@@ -2,3 +2,14 @@
 #include <stdlib.h>
 
 
+typedef struct {
+    char name[56];
+    float grade;
+    unsigned int aem;
+} student;
+
+typedef struct {
+    student data;
+    treeNode *nodeLeft;
+    treeNode *nodeRight;
+} treeNode;
