a5a524ae950fa9005c71e121fd55a6ba4116f176
Antonis Anagnostou <anagnostouantonis78@gmail.com>
2023-05-15 00:04:08 +0300

    Define functions to create and insert nodes into the tree

--- semester-2/data-structures/assignments/second/exe1.c
+++ semester-2/data-structures/assignments/second/exe1.c
@@ -14,6 +14,9 @@ typedef struct treeNode {
     struct treeNode *nodeRight;
 } treeNode;
 
+void nodeInsert (treeNode *root, treeNode *node);
+treeNode *nodeCreate (student *stud);
+
 int main(void) {
     // Variable declarations
     int i=1;
@@ -21,4 +24,22 @@ int main(void) {
     student *studentFile;
     treeNode *root;
     return 0;
+}
+
+void nodeInsert (treeNode *root, treeNode *node) {
+    if (root->data->aem > node->data->aem) {
+        if (root->nodeLeft != NULL) nodeInsert(root->nodeLeft, node);
+        if (root->nodeLeft == NULL) root->nodeLeft = node;
+    } else {
+        if (root->nodeRight != NULL) nodeInsert(root->nodeRight, node);
+        if (root->nodeRight == NULL) root->nodeRight = node;
+    }
+}
+
+treeNode *nodeCreate (student *stud) {
+    treeNode *node = malloc(sizeof(treeNode));
+    node->data = stud;
+    node->nodeLeft = NULL;
+    node->nodeRight = NULL;
+    return node;
 }
